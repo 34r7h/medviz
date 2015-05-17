b0ed85a6e3a8427bccfda38ad5fff2f66a85a6fc
irthos <irth03@gmail.com>
2015-05-17 13:21:18 -0700

    massive api overload with simplicity crud

--- app/scripts/components/admin/admin-f.js
+++ app/scripts/components/admin/admin-f.js
@@ -11,6 +11,240 @@ angular.module('medviz')
         'use strict';
 
         // INITIALIZATION
+        var uploadArray = [
+            {
+                "FIELD1":1,
+                "ZONE":"",
+                "CITY":"Aataouia",
+                "MUNICIPALITY":"Aataouia",
+                "DOCTOR NAME":"Id Ahmad Hicham",
+                "SPECIALITY":"MG",
+                "SECTOR FIELD":"HOPITAL LOCAL ATTAOUI",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Laaroussi Nadia",
+                "SPECIALITY":"MG (Urgences) ",
+                "SECTOR FIELD":"HOPITAL LOCAL ATTAOUI",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Laglaoui Aicha",
+                "SPECIALITY":"MG (Urgences) ",
+                "SECTOR FIELD":"HOPITAL LOCAL ATTAOUI",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Lahmidi Salwa",
+                "SPECIALITY":"MG",
+                "SECTOR FIELD":"HOPITAL LOCAL ATTAOUI",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Lemdilki Fatna",
+                "SPECIALITY":"MG",
+                "SECTOR FIELD":"HOPITAL LOCAL ATTAOUI",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Lemkhanet Ilham",
+                "SPECIALITY":"MG",
+                "SECTOR FIELD":"HOPITAL LOCAL ATTAOUI",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Mahboub",
+                "SPECIALITY":"MG (Urgences) ",
+                "SECTOR FIELD":"HOPITAL LOCAL ATTAOUI",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Mouti Amina",
+                "SPECIALITY":"MG ",
+                "SECTOR FIELD":"HOPITAL LOCAL ATTAOUI",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Touriki Youssef",
+                "SPECIALITY":"MG ",
+                "SECTOR FIELD":"HOPITAL LOCAL ATTAOUI",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Ouajou Meriem",
+                "SPECIALITY":"MG ",
+                "SECTOR FIELD":"CENTRE DE SANTE ATTAOUIA",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Tirizite Naima",
+                "SPECIALITY":"MG ",
+                "SECTOR FIELD":"CENTRE DE SANTE ATTAOUIA",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"",
+                "SPECIALITY":"MG ",
+                "SECTOR FIELD":"CENTRE DE SANTE ATTAOUIA",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"",
+                "SPECIALITY":"",
+                "SECTOR FIELD":"",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":2,
+                "ZONE":"",
+                "CITY":"AFOURAR ",
+                "MUNICIPALITY":"AFOURAR ",
+                "DOCTOR NAME":"El Aidi Zineb",
+                "SPECIALITY":"MG ",
+                "SECTOR FIELD":"CENTRE DE SANTE AFOURAR",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"El Khaldi Mustapha",
+                "SPECIALITY":"MG ",
+                "SECTOR FIELD":"CENTRE DE SANTE AFOURAR",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Najim Ahmed",
+                "SPECIALITY":"MG ",
+                "SECTOR FIELD":"CENTRE DE SANTE AFOURAR",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"Ezzahiri",
+                "SPECIALITY":"MG ",
+                "SECTOR FIELD":"CENTRE DE SANTE TIMOULILT ",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"",
+                "DOCTOR NAME":"",
+                "SPECIALITY":"",
+                "SECTOR FIELD":"",
+                "ADDRESS":"",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":3,
+                "ZONE":"",
+                "CITY":"AGADIR",
+                "MUNICIPALITY":"CENTRE VILLE",
+                "DOCTOR NAME":"Abadi Karim",
+                "SPECIALITY":"MG ",
+                "SECTOR FIELD":"CABINET PRIVE",
+                "ADDRESS":"AV , MARRAKECH ",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"CENTRE VILLE",
+                "DOCTOR NAME":"Abadi Laila",
+                "SPECIALITY":"",
+                "SECTOR FIELD":"CABINET PRIVE",
+                "ADDRESS":"Rue de Marrakech , Imm ACHROUK",
+                "CONTACT NUMBER":""
+            },
+            {
+                "FIELD1":null,
+                "ZONE":"",
+                "CITY":"",
+                "MUNICIPALITY":"Talborjt",
+                "DOCTOR NAME":"Abassor Aicha",
+                "SPECIALITY":"",
+                "SECTOR FIELD":"CABINET PRIVE",
+                "ADDRESS":"34, Rue Ibn Toumart",
+                "CONTACT NUMBER":""
+            }
+        ];
+
         var dataRef = Data.ref;
         var dataArray = $firebaseArray(dataRef);
         console.log('Admin Factory Injected With: ',arguments);
@@ -21,7 +255,34 @@ angular.module('medviz')
             createArray.$add(props);
             // TODO add to index
         }
-        // ('users', {name:'name', role:'admin'});
+        // ('doctors', newDoc);
+
+        (function uploadDocs(){
+            var uploadRef = dataRef.child('test');
+            var docCity = '';
+            angular.forEach(uploadArray, function(doctor, key){
+                if(doctor['DOCTOR NAME'].length > 0){
+                    if (doctor.CITY.length > 0){
+                        docCity = doctor.CITY.toLowerCase();
+                    }
+                    var newDoc = {};
+                    angular.forEach(doctor, function(entry, key){
+                        newDoc[key.toLowerCase()] = entry;
+                        newDoc.city = docCity;
+                        var uploadRef = dataRef.child('doctors');
+                        var uploadArray = $firebaseArray(uploadRef);
+                        uploadArray.$add(newDoc, function(error, data){
+                            if(error){
+                                console.log('blimey, somethings fucked in the uploading');
+                            } else {
+                                console.log(data);
+                            }
+                        });
+                    })
+
+                }
+            })
+         })();
 
         function update(type, id, ob){
             var updateRef = dataRef.child(type);
